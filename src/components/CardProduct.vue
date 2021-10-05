e0514673b114d7e58822e70c8399234213f5356b
unknown <saddam.aldriansyah@gmail.com>
2021-10-05 19:17:02 +0700

    Test Case Front End Vuejs

---
+++ src/components/CardProduct.vue
@@ -0,0 +1,30 @@
+<template>
+  
+    <div class="in-content">
+      <span v-if="product.stock < 10">
+      <p class="number color1">{{product.stock}}</p>
+      </span>
+      <span v-else>
+      <p class="number color2">{{product.stock}}</p>
+      </span>
+
+        <div class="desc-content">
+          <p class="name-content">{{product.name}}</p>
+            <div class="description">
+              <p class="left">Total Sold</p>
+              <p class="right default">{{product.stock}}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+name:"CardProduct",
+props:['product']
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home">
+
+    <div class="container">
+      <h1>Products</h1>
+        <div class="content" v-for="product in products" :key="product.id" > 
+
+          <CardProduct :product="product"/>
+
+          </div>
+      </div>
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import CardProduct from '@/components/CardProduct.vue';
+import axios from 'axios';
+
+export default {
+  name: 'Home',
+  components: {
+    CardProduct,
+  },
+  data(){
+    return {
+      products:[],
+      sales:[]
+    }
+  },
+  methods:{
+    setProduct(data){
+      this.products = data,
+      this.sales = data
+    }
+  },
+
+  mounted() {
+    axios
+      .get("http://localhost:3000/products")
+      .then((response) => this.setProduct(response.data))
+      .catch((error) => console.log(error));
+  },
+}
+</script>
